1bc9e1d850b9bd022ea2fef9c1fbac6de5282f4e
Richard Whitcomb <rwhitcomb@twitter.com>
2016-02-06 16:27:19 -0500

    Add a real test

--- src/test/scala/com/twitter/lsh/LshTest.scala
+++ src/test/scala/com/twitter/lsh/LshTest.scala
@@ -4,6 +4,8 @@ import com.twitter.lsh.vector._
 import com.twitter.util.Await
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.io.Source
+
 object LshSetup {
   val vectors = Array(
     IndexedVector(1L, LshVector(Array(1.0, 2.0, 3.0, 4.0))),
@@ -41,6 +43,37 @@ class EuclideanLshTest extends WordSpec with Matchers {
 
       assert(lshResults.size == 1)
       assert(lshResults.keySet.contains(1L))
+    }
+  }
+
+  "Lsh on Real Data" should {
+    val vectors = Source
+      .fromFile("src/test/resources/data/boston_housing_prices.csv")
+      .getLines()
+      .toArray
+      .tail
+      .zipWithIndex
+      .map{case(row, idx) => IndexedVector(idx, LshVector(row.split(",").map(_.toDouble)))}
+
+    val expected = List(
+      ScoredResult(1, 1.0),
+      ScoredResult(47, 0.9995101972678899),
+      ScoredResult(49, 0.9991467915947881),
+      ScoredResult(2, 0.9988605961050612),
+      ScoredResult(87, 0.9988242521933062),
+      ScoredResult(337, 0.9987435788329281),
+      ScoredResult(88, 0.9986785229239838),
+      ScoredResult(85, 0.9986721414567372),
+      ScoredResult(340, 0.9985698872374255),
+      ScoredResult(91, 0.9984498075371634)
+    )
+
+    "find vectors" in {
+      val lsh = EuclideanLsh.build(LshParams(5, 10, 1.0, 13), vectors)
+
+      val results = Await.result(lsh.queryKey(1, 10))
+
+      assert(results === expected)
     }
   }
 }
